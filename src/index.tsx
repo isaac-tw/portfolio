5b3d5186f2ac34cc1f57f608b37d3dc5b8bc0c02
Isaac Huang <isaac.huang.tw@gmail.com>
2022-10-09 14:32:47 +0800

    Add react router and Handling Not Found Errors
    https://reactrouter.com/en/main/start/tutorial#handling-not-found-errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    // element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

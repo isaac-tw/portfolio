a8cf0ff473a644859e97814b44ea1ec2c2b741fb
Isaac Huang <isaac.huang.tw@gmail.com>
2022-11-12 14:46:59 +0800

    Add thumbnails

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,27 @@
+export default function ProjectCard(props: {
+  id: string,
+  name: string,
+  subtitle: string,
+  year: number,
+  duration: string,
+}) {
+  return (
+    <div className='card'>
+      <div className='image-section'>
+        <img
+          className='img --front'
+          src={require(`../assets/${props.id}/${props.id}_thumbnail-01_IsaacHuang.jpg`)}
+          alt={`${props.name}`}
+        />
+        <img
+          className='img'
+          src={require(`../assets/${props.id}/${props.id}_thumbnail-02_IsaacHuang.jpg`)}
+          alt={`${props.name}`}
+        />
+      </div>
+      <div className='title rfs-main'>{props.name}</div>
+      <div className='subtitle rfs-sub'>{props.subtitle}</div>
+
+    </div>
+  );
+}

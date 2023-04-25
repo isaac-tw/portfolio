11474fea07cd5b3238fcf976d8b80a3293c2d0a5
Isaac Huang <isaac.huang.tw@gmail.com>
2023-04-25 23:33:36 +0800

    Remove default stuffs and update TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,6 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/configure';
 
-// TODO:
-// (DONE) 1. Set route to get project -> /work/luggageplus
-// (DONE) 2. GIF
-// (DONE) 3. Under construction
-// (DONE) 4. Propotion
-// (DONE) 5. Favicon
-// (DONE) 6. Error page
-// (DONE) 7. Linter
-// 8. Remove all default stuffs
-// 9. Update all ts in codebase
-// (DONE) 10. mailto
-// (SKIP) 11. reCAPTCHA
-// (DONE) 12. Lazyload offset
-// (DONE) 13. Change @media to @mixin
-
 function App(): JSX.Element {
   return (
     <div className='App'>

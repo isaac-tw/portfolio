6d254c77ae758d5d75c7ba97b5c2ad7732cfbeda
Isaac Huang <isaac.huang.tw@gmail.com>
2023-06-05 13:29:07 +0000

    Add rebalance

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,10 +4,12 @@ import deepsense from './deepsense';
 import luggageplus from './luggageplus.json';
 import magprint from './magprint.json';
 import oAssist from './o-assist';
+import rebalance from './rebalance';
 import remoteControlRedesign from './remote-control-redesign.json';
 import searchingBall from './searching-ball.json';
 
 const projects = {
+  rebalance,
   'remote-control-redesign': remoteControlRedesign,
   luggageplus,
   'o-assist': oAssist,
---
+++ src/data/rebalance.js
@@ -0,0 +1,23 @@
+import UnderConstruction from '../components/shared/UnderConstruction';
+
+const details = [
+  [
+    {
+      file: 'details-component',
+      format: 'component',
+      component: UnderConstruction
+    }
+  ]
+];
+
+const reBalance = {
+  id: 'rebalance',
+  name: 'ReBalance',
+  subtitle: 'Have/Give a massage anywhere (UI/UX)',
+  year: 2022,
+  duration: '8 Months',
+  note: '',
+  details
+};
+
+export default reBalance;

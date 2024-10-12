2bce2849a03e6da8fe712e7804d24e092a013bd6
Isaac Huang <isaac.huang.tw@gmail.com>
2024-10-12 20:27:16 +0800

    Remove reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './styles/styles.scss';
 import 'photoswipe/dist/photoswipe.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './data/theme';
 import ReactGA from 'react-ga4';
@@ -33,8 +32,3 @@ root.render(
     </React.StrictMode>
   </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

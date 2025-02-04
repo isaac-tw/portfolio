c070e7ce5e03b5a903ad7defb2bac91140ac6f8d
Isaac Huang <isaac.huang.tw@gmail.com>
2025-02-04 14:56:59 +0800

    Fix TypeError: styled_default is not a function

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build'
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^@mui\/icons-material\/(.*)/,
+        replacement: "@mui/icons-material/esm/$1",
+      },
+    ],
   }
 })

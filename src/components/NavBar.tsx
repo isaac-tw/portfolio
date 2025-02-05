a0be66e7651ff8cb6e093007ef9dbe453dc52f4c
Isaac Huang <isaac.huang.tw@gmail.com>
2025-02-05 08:01:03 +0800

    Add GitHub to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,32 +1,68 @@
 import { Link, NavLink } from 'react-router-dom';
+import { IconButton } from '@mui/material';
+import { Email, GitHub } from '@mui/icons-material';
 
-export default function NavBar(): JSX.Element {
-  const renderIcon = (): JSX.Element => (
-    <Link to='/' className='icon'>
-      <div className='first-name'>Wei-Chun&nbsp;</div>
-      <div className='last-name'>Huang</div>
-    </Link>
-  );
-  const renderTabs = (): JSX.Element => (
-    <div className='tabs'>
-      <nav className='elements'>
-        <NavLink to='/'>Work</NavLink>
-        <div className='divider'>/</div>
-        <NavLink to='misc'>misc.</NavLink>
-        <div className='divider'>/</div>
-        <NavLink to='about'>About</NavLink>
-        <div className='divider'>/</div>
-        <a href='mailto:isaac.huang.tw@gmail.com'>Contact</a>
-      </nav>
-    </div>
-  );
+const gitHubLink = 'https://github.com/isaac-tw';
+const mailtoLink = 'mailto:isaac.huang.tw@gmail.com';
 
-  return (
-    <div className='nav'>
-      <div className='nav__bar rfs-main'>
-        {renderIcon()}
-        {renderTabs()}
+const NameArea = () => (
+  <div className='navbar__name'>
+    <Link to='/' className='navbar__full-name'>
+      <div className='navbar__first-name'>
+        <span>Wei-Chun&nbsp;</span>
       </div>
+      <div className='navbar__last-name'>Huang</div>
+    </Link>
+    <div className='navbar__icon-buttons'>
+      <IconButton
+        aria-label='GitHub'
+        href={gitHubLink}
+        rel='noreferrer noopener'
+        target='_blank'
+      >
+        <GitHub fontSize='large' />
+      </IconButton>
+      <IconButton aria-label='Email' href={mailtoLink}>
+        <Email fontSize='large' />
+      </IconButton>
     </div>
+  </div>
+);
+
+const TabsArea = () => (
+  <div className='navbar__tabs'>
+    <nav className='navbar__tab-list'>
+      <NavLink to='/'>Work</NavLink>
+      <div className='navbar__tab-divider'>/</div>
+      <NavLink to='misc'>misc.</NavLink>
+      <div className='navbar__tab-divider'>/</div>
+      <NavLink to='about'>About</NavLink>
+
+      <div className='navbar__tab-divider navbar__tab-divider--responsive'>/</div>
+      <Link
+        aria-label='GitHub'
+        className='navbar__tab-link'
+        to={gitHubLink}
+        rel='noreferrer noopener'
+        target='_blank'
+      >
+        GitHub
+      </Link>
+      <div className='navbar__tab-divider navbar__tab-divider--responsive'>/</div>
+      <Link className='navbar__tab-link' to={mailtoLink}>
+        Contact
+      </Link>
+    </nav>
+  </div>
+);
+
+export default function NavBar() {
+  return (
+    <header className='navbar'>
+      <div className='navbar__container rfs-main'>
+        <NameArea />
+        <TabsArea />
+      </div>
+    </header>
   );
 }

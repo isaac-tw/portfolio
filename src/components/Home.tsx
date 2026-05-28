754d19d9dee9cbdd30138cd1b153ad6331cde3e7
Isaac Huang <isaac.huang.tw@gmail.com>
2026-05-28 22:33:09 +0200

    Track pageviews on React Router route changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,31 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import NavBar from './NavBar';
+
+function RoutePageviewTracker() {
+  const location = useLocation();
+  const isFirstRoute = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRoute.current) {
+      isFirstRoute.current = false;
+      return;
+    }
+
+    ReactGA.send({
+      hitType: 'pageview',
+      page: `${location.pathname}${location.search}`
+    });
+  }, [location.pathname, location.search]);
+
+  return null;
+}
 
 export default function Home() {
   return (
     <div>
+      <RoutePageviewTracker />
       <NavBar />
       <Outlet />
       <ScrollRestoration />
